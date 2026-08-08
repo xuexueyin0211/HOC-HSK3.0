78894c1beb02c8fc10e06ca65f79b5fa5151045a
Tuyet Ngan <ttmm.xueyin@gmail.com>
2026-08-08 13:04:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11552,7 +11552,7 @@ fetchJson(`./data/vocab/${safeLvl}.json`),
     fetchJson(`./data/grammar/${safeLvl}.json`),
     fetchJson(`./data/texts/${safeLvl}.json`),
     fetchJson(`./data/exercises/${safeLvl}.json`),
-    fetchJson(`./data/progress test/${safeLvl}.json`)
+    fetchJson(`./data/progress%20test/${safeLvl}.json`)
     ]);
 
     // Helper to find item matching lesson_id / lesson / id
